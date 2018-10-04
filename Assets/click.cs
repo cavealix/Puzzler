e16a54b8925b26c79efe549043ba7c08cc378d74
cavealix <cavealix@gmail.com>
2018-10-04 18:12:42 -0500

    Final
    Additions of final tweaks for project to be accepted.

---
+++ Assets/click.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click : MonoBehaviour {
+
+    // The gameobject that has the GameLogic.cs script attached.
+    public GameLogic gameLogic;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
